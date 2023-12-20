38f3358816796a2ced2e9f0bfa3c9a66bfde914d
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-12-20 01:45:25 +0100

    Added new page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,14 @@
 <Header />
 
 <main>
-  <section class="page2-sponsor-page">
-    <div class="text-area">
-        <h2 class="page2-numbers-header page2-sponsor-text">
+  <section class="sponsorpage">
+    <div class="h2">
+        <h2 class="sponsorpage-span-h2">
             Become a <span class="yellow">sponsor</span>
         </h2>
     </div>
-    <div class="page2-options-wrapper">
-        <div class="page2-option page2-option1">
+    <div class="sponsorpage-wrapper">
+        <div class="sponsoroptions material">
             <svg viewBox="0 0 149 198" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M142.515 0.00614879C149.727 0.0915737 149.942 0.333528 147.852 22.5583C143.991 63.6479 139.988 104.595 136.045 145.599C134.83 158.214 133.616 170.828 132.406 183.457C131.2 195.986 130.626 197.808 126.631 197.894C116.059 198.093 105.483 197.951 94.9109 197.951L64.907 197.951L64.907 197.979C45.1876 197.979 25.4724 198.008 5.753 197.965C-0.75149 197.965 -0.93599 196.897 1.26936 176.281C6.61111 126.307 11.9443 76.3336 17.226 26.3029C18.2814 16.2797 20.002 10.1005 23.4258 10.4992C24.846 10.67 26.2748 9.8728 27.7035 9.77314C51.4132 8.13582 75.1228 6.0998 98.8325 5.1174C113.412 4.50519 127.935 -0.193176 142.515 0.00614879Z" fill=""/>
             </svg>
@@ -30,7 +30,7 @@
                 </button>
             </div>
         </div>
-        <div class="page2-option page2-option2">
+        <div class="sponsoroptions financial">
             <svg viewBox="0 0 149 198" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M142.515 0.00614879C149.727 0.0915737 149.942 0.333528 147.852 22.5583C143.991 63.6479 139.988 104.595 136.045 145.599C134.83 158.214 133.616 170.828 132.406 183.457C131.2 195.986 130.626 197.808 126.631 197.894C116.059 198.093 105.483 197.951 94.9109 197.951L64.907 197.951L64.907 197.979C45.1876 197.979 25.4724 198.008 5.753 197.965C-0.75149 197.965 -0.93599 196.897 1.26936 176.281C6.61111 126.307 11.9443 76.3336 17.226 26.3029C18.2814 16.2797 20.002 10.1005 23.4258 10.4992C24.846 10.67 26.2748 9.8728 27.7035 9.77314C51.4132 8.13582 75.1228 6.0998 98.8325 5.1174C113.412 4.50519 127.935 -0.193176 142.515 0.00614879Z" fill=""/>
             </svg>
@@ -47,7 +47,7 @@
                 </button>
             </div>
         </div>
-        <div class="page2-option page2-option3">
+        <div class="sponsoroptions clothing">
             <svg viewBox="0 0 149 198" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M142.515 0.00614879C149.727 0.0915737 149.942 0.333528 147.852 22.5583C143.991 63.6479 139.988 104.595 136.045 145.599C134.83 158.214 133.616 170.828 132.406 183.457C131.2 195.986 130.626 197.808 126.631 197.894C116.059 198.093 105.483 197.951 94.9109 197.951L64.907 197.951L64.907 197.979C45.1876 197.979 25.4724 198.008 5.753 197.965C-0.75149 197.965 -0.93599 196.897 1.26936 176.281C6.61111 126.307 11.9443 76.3336 17.226 26.3029C18.2814 16.2797 20.002 10.1005 23.4258 10.4992C24.846 10.67 26.2748 9.8728 27.7035 9.77314C51.4132 8.13582 75.1228 6.0998 98.8325 5.1174C113.412 4.50519 127.935 -0.193176 142.515 0.00614879Z" fill=""/>
             </svg>
@@ -71,6 +71,7 @@
 <Footer />
 
 <style>
+/* Common styles for all screen sizes */
 * {
   box-sizing: border-box;
   margin: 0;
@@ -88,6 +89,10 @@ h2 {
   color: var(--justWhite);
 }
 
+.sponsorpage-span-h2 {
+  margin-bottom: 1em;
+}
+
 p {
   font-family: var(--copy-font);
   font-size: 1em;
@@ -96,6 +101,53 @@ p {
 
 .yellow {
   color: var(--candelLight);
+}
+
+.sponsorpage {
+  height: max-content;
+  width: 100%;
+  padding-top: 7em;
+  overflow-x: hidden;
+}
+
+.sponsorpage-wrapper {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  height: 77%;
+  width: 100%;
+}
+
+.sponsoroptions {
+  position: relative;
+  height: 95%;
+  width: 100%;
+}
+
+.material svg {
+  position: relative;
+  fill: var(--candelLight);
+}
+
+.financial svg {
+  fill: var(--pictonBlue);
+}
+
+.clothing svg {
+  fill: var(--jungleGreen);
+}
+
+.options-text-wrapper {
+  position: absolute;
+  top: 10%;
+  left: 18%;
+  width: 60%;
+}
+
+h4 {
+  font-size: 2em;
+  font-family: var(--paragraph-font);
+  margin-bottom: 1em;
 }
 
 .sponsor-button {
@@ -125,49 +177,9 @@ p {
   transform: translateY(1px);
 }
 
-.page2-sponsor-page {
-  height: max-content;
-  width: 100%;
-  padding-top: 7em;
-  overflow-x: hidden;
-}
-
-.page2-sponsor-text {
-  margin-bottom: 1em;
-}
-
-.page2-options-wrapper {
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  height: 77%;
-  width: 100%;
-}
-
-.page2-option {
-  position: relative;
-  height: 95%;
-  width: 100%;
-}
-
-.page2-option1 svg {
-  position: relative;
-  fill: var(--candelLight);
-}
-
-.page2-option2 svg {
-  fill: var(--pictonBlue);
-}
-
-.page2-option3 svg {
-  fill: var(--jungleGreen);
-}
-
-.options-text-wrapper {
-  position: absolute;
-  top: 10%;
-  left: 18%;
-  width: 60%;
+.sponsor-page-button {  
+  z-index: 3;
+  margin-top: 20%;
 }
 
 .black {
@@ -192,21 +204,25 @@ p {
   fill: var(--justWhite);
 }
 
-.page2-options-wrapper h4 {
-  font-size: 2em;
-  font-family: var(--paragraph-font);
-  margin-bottom: 1em;
-}
-
-.sponsor-page-button {  
-  z-index: 3;
-  margin-top: 20%;
-}
-
 /* MEDIA QUERY 600PX */
-@media only screen and (max-width: 600px){
-section {
-  padding: 0 7vw;
+@media only screen and (max-width: 600px) {
+  section {
+    padding: 0 3vw; /* Adjusted padding for smaller screens */
+  }
+
+  /* Additional mobile-specific styles can be added here */
+  h2 {
+    font-size: 2.5em; /* Decreased font size for better mobile readability */
+  }
+
+  .options-text-wrapper {
+    left: 5%; /* Adjusted position for better alignment on mobile */
+    width: 90%; /* Increased width for better mobile layout */
+  }
+
+  .sponsor-button {
+    padding: 0.8em 1.5em; /* Smaller padding for better mobile appearance */
+  }
 }
-}
+
 </style>
