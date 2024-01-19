b888753664bccd34b01141108a352eae7c51a311
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2024-01-19 19:52:12 +0100

    :lipstick:
    -REFACTOR
    -NEW UI 80% IMPLEMENTED
    -MEDIA QUERIES

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,300 +3,260 @@
   import Footer from "$lib/components/Footer.svelte";
 </script>
 
-<Header />
-
 <main>
-  <section class="sponsorpage">
-    <div class="h2">
-        <h2 class="sponsorpage-span-h2">
-            Become a <span class="yellow">sponsor</span>
-        </h2>
+  <section id="hero1" class="hero">
+    <div class="inner">
+      <h2 class="sponsorpage-span-h2">
+        Become a <span class="yellow">sponsor</span>
+      </h2>
+      <a href="#sponsorpage">
+        <svg class="arrow" xmlns="http://www.w3.org/2000/svg" fill="#ff0" height="50" viewBox="0 -960 960 960" width="50">
+          <path d="m480-320 160-160-56-56-64 64v-168h-80v168l-64-64-56 56 160 160Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+        </svg>
+      </a>
     </div>
-    <div class="sponsorpage-wrapper">
-        <div class="sponsoroptions material">
-            <svg viewBox="0 0 149 198" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M142.515 0.00614879C149.727 0.0915737 149.942 0.333528 147.852 22.5583C143.991 63.6479 139.988 104.595 136.045 145.599C134.83 158.214 133.616 170.828 132.406 183.457C131.2 195.986 130.626 197.808 126.631 197.894C116.059 198.093 105.483 197.951 94.9109 197.951L64.907 197.951L64.907 197.979C45.1876 197.979 25.4724 198.008 5.753 197.965C-0.75149 197.965 -0.93599 196.897 1.26936 176.281C6.61111 126.307 11.9443 76.3336 17.226 26.3029C18.2814 16.2797 20.002 10.1005 23.4258 10.4992C24.846 10.67 26.2748 9.8728 27.7035 9.77314C51.4132 8.13582 75.1228 6.0998 98.8325 5.1174C113.412 4.50519 127.935 -0.193176 142.515 0.00614879Z" fill=""/>
-            </svg>
-            <div class="options-text-wrapper">
-                <h4 class="black">MATERIAL</h4>
-                <p class="black">Sponsor football materials, change the Game! Your generous donation can provide crucial equipment to aspiring athletes. 
-                  <br>
-                  Help us make a difference today.</p>
-                  <a href="/payment">
-                    <button class="sponsor-button sponsor-page-button green">
-                      Choose sponsor
-                  </a>
-                    <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1.1417 0.25L0 1.48375L3.7085 5.5L0 9.51625L1.1417 10.75L6 5.5L1.1417 0.25Z" fill=""/>
-                    </svg>
-            </div>
-        </div>
-        <div class="sponsoroptions financial">
-            <svg viewBox="0 0 149 198" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M142.515 0.00614879C149.727 0.0915737 149.942 0.333528 147.852 22.5583C143.991 63.6479 139.988 104.595 136.045 145.599C134.83 158.214 133.616 170.828 132.406 183.457C131.2 195.986 130.626 197.808 126.631 197.894C116.059 198.093 105.483 197.951 94.9109 197.951L64.907 197.951L64.907 197.979C45.1876 197.979 25.4724 198.008 5.753 197.965C-0.75149 197.965 -0.93599 196.897 1.26936 176.281C6.61111 126.307 11.9443 76.3336 17.226 26.3029C18.2814 16.2797 20.002 10.1005 23.4258 10.4992C24.846 10.67 26.2748 9.8728 27.7035 9.77314C51.4132 8.13582 75.1228 6.0998 98.8325 5.1174C113.412 4.50519 127.935 -0.193176 142.515 0.00614879Z" fill=""/>
-            </svg>
-            <div class="options-text-wrapper">
-                <h4>FINANCIAL</h4>
-                <p>Support Financially, Empower Dreams! Your generous donation can provide vital financial support to individuals in need. 
-                  <br>
-                  Help us make a difference today</p>
-                    <a href="/payment">
-                      <button class="sponsor-button sponsor-page-button">
-                        Choose sponsor
-                    </a>
-                    <!-- <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1.1417 0.25L0 1.48375L3.7085 5.5L0 9.51625L1.1417 10.75L6 5.5L1.1417 0.25Z" fill="#051A1A"/>
-                    </svg> -->
-            </div>
-        </div>
-        <div class="sponsoroptions clothing">
-            <svg viewBox="0 0 149 198" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M142.515 0.00614879C149.727 0.0915737 149.942 0.333528 147.852 22.5583C143.991 63.6479 139.988 104.595 136.045 145.599C134.83 158.214 133.616 170.828 132.406 183.457C131.2 195.986 130.626 197.808 126.631 197.894C116.059 198.093 105.483 197.951 94.9109 197.951L64.907 197.951L64.907 197.979C45.1876 197.979 25.4724 198.008 5.753 197.965C-0.75149 197.965 -0.93599 196.897 1.26936 176.281C6.61111 126.307 11.9443 76.3336 17.226 26.3029C18.2814 16.2797 20.002 10.1005 23.4258 10.4992C24.846 10.67 26.2748 9.8728 27.7035 9.77314C51.4132 8.13582 75.1228 6.0998 98.8325 5.1174C113.412 4.50519 127.935 -0.193176 142.515 0.00614879Z" fill=""/>
-            </svg>
-            <div class="options-text-wrapper">
-                <h4>CLOTHING</h4>
-                <p>Sponsor lothing, transform Lives! Your generous donation can provide essential clothing to those in need.
-                  <br>
-                  Help us make a difference today.</p>
-                  <a href="/payment">
-                    <button class="sponsor-button sponsor-page-button">
-                      Choose sponsor
-                  </a>
-                    <!-- <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1.1417 0.25L0 1.48375L3.7085 5.5L0 9.51625L1.1417 10.75L6 5.5L1.1417 0.25Z" fill="#051A1A"/>
-                    </svg> -->
-            </div>
-        </div>
+  </section>
+
+  <div class="sponsorpage-wrapper">
+    <div class="sponsoroptions material">
+      <svg width="100%" height="539" viewBox="0 0 1512 539" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1513 0 0 69.428v406.284L1513 539V0Z" fill="#FFDA1D" />
+      </svg>
+      <div class="options-text-wrapper">
+        <h4 class="black">MATERIAL</h4>
+        <p class="black">
+          Sponsor football materials, change the Game! Your generous donation can provide crucial equipment to aspiring athletes. <br />
+          Help us make a difference today.
+        </p>
+        <a href="/payment">
+          <button class="sponsor-button sponsor-page-button green">
+            Choose sponsor
+          </button>
+        </a>
+      </div>
     </div>
-</section>
+
+    <div class="sponsoroptions financial">
+      <svg width="100%" height="530" viewBox="0 0 1512 530" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M-5 0L1536 68.2689V467.769L-5 530V0Z" fill="#3DBAE1" />
+      </svg>
+      <div class="options-text-wrapper">
+        <h4>FINANCIAL</h4>
+        <p>
+          Support Financially, Empower Dreams! Your generous donation can provide vital financial support to individuals in need. <br />
+          Help us make a difference today
+        </p>
+        <a href="/payment">
+          <button class="sponsor-button sponsor-page-button">
+            Choose sponsor
+          </button>
+        </a>
+      </div>
+    </div>
+
+    <div class="sponsoroptions clothing">
+      <svg width="100%" height="557" viewBox="0 0 1508 557" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1508 0L-5 71.7468V491.598L1508 557V0Z" fill="#163A3A" />
+      </svg>
+      <div class="options-text-wrapper">
+        <h4>CLOTHING</h4>
+        <p>
+          Sponsor clothing, transform Lives! Your generous donation can provide essential clothing to those in need.<br />
+          Help us make a difference today.
+        </p>
+        <a href="/payment">
+          <button class="sponsor-button sponsor-page-button">
+            Choose sponsor
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+
 </main>
 
-<Footer />
-
 <style>
-/* Common styles for all screen sizes */
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-section {
-  padding: 0 6em;
-  overflow: hidden;
-}
-
-h2 {
-  font-family: var(--paragraph-font);
-  font-size: 3.5em;
-  color: var(--justWhite);
-}
-
-.sponsorpage-span-h2 {
-  margin-bottom: 1em;
-}
-
-p {
-  font-family: var(--copy-font);
-  font-size: 1em;
-  color: var(--justWhite);
-}
-
-.yellow {
-  color: var(--candelLight);
-}
-
-.sponsorpage {
-  height: max-content;
-  width: 100%;
-  padding-top: 7em;
-  overflow-x: hidden;
-}
-
-.sponsorpage-wrapper {
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  height: 77%;
-  width: 100%;
-}
-
-.sponsoroptions {
-  position: relative;
-  height: 95%;
-  width: 100%;
-}
-
-.material svg {
-  position: relative;
-  fill: var(--candelLight);
-}
-
-.financial svg {
-  fill: var(--pictonBlue);
-}
-
-.clothing svg {
-  fill: var(--jungleGreen);
-}
-
-.options-text-wrapper {
-  position: absolute;
-  top: 10%;
-  left: 18%;
-  width: 60%;
-}
-
-h4 {
-  font-size: 2em;
-  font-family: var(--paragraph-font);
-  margin-bottom: 1em;
-}
-
-.sponsor-button {
-  text-decoration: none;
-  color: var(--charcoal);
-  background-color: var(--candelLight);
-  font-family: var(--copy-font);
-  font-weight: bold;
-  border: none;
-  border-radius: 3em;
-  padding: 1em 2em;
-  text-transform: uppercase;
-  transition: .2s;
-  cursor: pointer;
-}
-
-.sponsor-button:hover {
-  background-color: var(--button-hover-color);
-  color: var(--charcoal);
-}
-
-.sponsor-button:hover svg {
-  transform: translateY(1px) translateX(3px);
-}
-
-.sponsor-button svg {
-  transform: translateY(1px);
-}
-
-.sponsor-page-button {  
-  z-index: 3;
-  margin-top: 20%;
-}
-
-.black {
-  color: var(--charcoal);
-}
-
-.green {
-  background-color: var(--justWhite);
-  color: var(--charcoal);
-}
-
-/* .green svg {
-  fill: var(--charcoal);
-} */
-
-.green:hover {
-  background-color: var(--pictonBlue);
-  color: var(--justWhite);
-}
-
-/* .green:hover svg {
-  fill: var(--justWhite);
-} */
-
-
-/* MEDIA QUERY FOR TABLET */
-@media only screen and (max-width: 1050px) {
-  .sponsorpage-wrapper {
-    flex-direction: column; /* Stack items vertically on tablets */
+  /* Common styles for all screen sizes */
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
 
-  .sponsoroptions {
-    width: 100%;
-    margin: 20px 0; /* Increased margin for better spacing */
-    position: relative;
-    text-align: center; /* Center text within the sponsor options */
-  }
-
-  .options-text-wrapper {
-    width: 80%; /* Adjusted width for better text wrapping */
-    margin: 0 auto; /* Center text within options-text-wrapper */
-  }
-
-  .sponsoroptions h4 {
-    margin: 10px 0px 20px;
-    margin-right: 39vw;
-  }
-
-  .sponsoroptions p {
-    margin: 0; /* Remove default margin */
-    text-align: justify; /* Justify text for better readability */
-    margin-right: 10vw;
-  }
-
-  .sponsor-button {
-    padding: 0.8em 1.5em;
-    display: inline-block;
-    margin: 15px 0;
-    margin-right: 39vw;
-  }
-
-  .sponsor-button svg {
-    vertical-align: middle;
-    margin-left: 5px;
-  }
-}
-
-
-
-/* MEDIA QUERY FOR MOBILE */
-@media only screen and (max-width: 600px) {
   section {
-    padding: 0 1em; /* Adjusted padding for smaller screens */
+    padding: 0 6em;
+    overflow: hidden;
+  }
+
+  .arrow {
+    fill: var(--candelLight);
+  }
+
+  h2 {
+    font-family: var(--paragraph-font);
+    font-size: 5.5em;
+    color: var(--justWhite);
+  }
+
+  .sponsorpage-span-h2 {
+    margin-top: 4em;
+  }
+
+  p {
+    font-family: var(--copy-font);
+    font-size: 1em;
+    color: var(--justWhite);
+  }
+
+  .yellow {
+    color: var(--candelLight);
   }
 
   .sponsorpage-wrapper {
-    flex-direction: column; /* Stack items vertically on smaller screens */
-    height: auto; /* Allow the height to adjust based on content */
+    position: relative;
+    display: flex;
+    flex-direction: column;
   }
 
   .sponsoroptions {
-    width: 100%; /* Occupy full width on mobile */
-    margin: 10px 0; /* Add margin between stacked items */
-    position: relative; /* Ensure relative positioning */
+    position: relative;
+  }
+
+  .material svg,
+  .financial svg,
+  .clothing svg {
+    position: relative;
+    width: 100%;
+    height: auto;
   }
 
   .options-text-wrapper {
-    position: absolute; /* Position text within each sponsor option */
-    top: 46%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /* Centering trick */
-    width: 90%; /* Adjusted width */
-    text-align: center; /* Center text */
+    position: absolute;
+    top: 20%;
+    left: 18%;
+    width: 60%;
   }
 
-  .sponsoroptions h4,
-  .sponsoroptions p {
-    text-align: left; /* Center text on mobile */
-    margin-bottom: 10px; /* Add spacing between elements */
-    margin-left: 30px;
+  h4 {
+    font-size: 2em;
+    font-family: var(--paragraph-font);
+    margin-bottom: 1em;
   }
 
-  /* Adjustments for buttons and their SVG icons */
   .sponsor-button {
-    padding: 0.8em 1.5em; /* Smaller padding for better mobile appearance */
-    display: block; /* Display buttons as block elements for stacking */
-    margin: 10px auto; /* Center buttons */
-    margin-left: 30px;
+    text-decoration: none;
+    color: var(--charcoal);
+    background-color: var(--candelLight);
+    font-family: var(--copy-font);
+    font-weight: bold;
+    border: none;
+    border-radius: 3em;
+    padding: 1em 2em;
+    text-transform: uppercase;
+    transition: .2s;
+    cursor: pointer;
   }
 
-  .sponsor-button svg {
-    vertical-align: middle; /* Center SVG icon vertically with text */
-    margin-left: 5px; /* Add spacing between text and icon */
+  .sponsor-button:hover {
+    background-color: var(--button-hover-color);
+    color: var(--charcoal);
   }
-}
 
+  .sponsor-page-button {
+    z-index: 3;
+    margin-top: 9%;
+  }
+
+  .black {
+    color: var(--charcoal);
+  }
+
+  .green {
+    background-color: var(--justWhite);
+    color: var(--charcoal);
+  }
+
+  .green:hover {
+    background-color: var(--pictonBlue);
+    color: var(--justWhite);
+  }
+
+  *,
+  *:after,
+  *:before {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  #hero1 {
+    background-image: url("brasilvoetbalveld.png");
+    background-size: cover;
+    background-position: center center;
+    background-attachment: fixed;
+  }
+
+  .hero {
+    text-align: center;
+    position: relative;
+    width: 100%;
+  }
+
+  .inner {
+    min-height: 600px;
+    position: relative;
+  }
+
+  @media screen and (max-width: 768px) {
+    section {
+      padding: 0 2em; /* Adjust padding for smaller screens */
+    }
+
+    .hero {
+      text-align: center;
+      padding: 2em 0; /* Adjust padding for smaller screens */
+    }
+
+    h2 {
+      font-size: 3em; /* Adjust font size for smaller screens */
+    }
+
+    .sponsorpage-span-h2 {
+      margin-top: 6em; /* Adjust margin for smaller screens */
+    }
+
+    .options-text-wrapper {
+      top: 15%; /* Adjust position for smaller screens */
+      left: 5%; /* Adjust position for smaller screens */
+      width: 90%; /* Adjust width for smaller screens */
+    }
+
+    p {
+      font-size: 0.9em; /* Adjust font size for smaller screens */
+    }
+
+    h4 {
+      font-size: 1.5em; /* Adjust font size for smaller screens */
+      margin-bottom: 0.5em; /* Adjust margin for smaller screens */
+    }
+
+    .sponsor-button {
+      padding: 0.8em 1.5em; /* Adjust padding for smaller screens */
+    }
+
+    .sponsor-page-button {
+      margin-top: 4%; /* Adjust margin for smaller screens */
+    }
+
+    .sponsoroptions {
+      margin-bottom: 2em; /* Add margin between sponsor options for smaller screens */
+    }
+
+    .material svg,
+    .financial svg,
+    .clothing svg {
+      width: 100%; /* Adjust width for smaller screens */
+      height: auto; /* Maintain aspect ratio for SVG on smaller screens */
+    }
+  }
 </style>
